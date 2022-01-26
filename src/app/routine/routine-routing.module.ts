63a97bc6693da400b57c98896403e56d506b7954
HarmandoHernandez <josearmandohv99@gmail.com>
2022-01-26 17:19:00 -0600

    Local routine deploy

--- src/app/routine/routine-routing.module.ts
+++ src/app/routine/routine-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 
-import { RoutineComponent } from './routine.component';
-import { AddRoutineComponent } from './add-routine/add-routine.component';
-import { ShowRoutineComponent } from './show-routine/show-routine.component';
-import { UpdateRoutineComponent } from './update-routine/update-routine.component';
-import { DeleteRoutineComponent } from './delete-routine/delete-routine.component';
+import { RoutineComponent } from './routine.component'
+import { AddRoutineComponent } from './add-routine/add-routine.component'
+import { ShowRoutineComponent } from './show-routine/show-routine.component'
+import { UpdateRoutineComponent } from './update-routine/update-routine.component'
+import { DeleteRoutineComponent } from './delete-routine/delete-routine.component'
 
 const routes: Routes = [
-  { path: '', component: RoutineComponent },
-  { path: '**', component: RoutineComponent }
+  { path: '', component: RoutineComponent }/* ,
+  { path: '**', component: RoutineComponent } */
   /* { path: '',   redirectTo: '/all', pathMatch: 'full' },
   { path: 'all', component: ShowRoutineComponent },
   { path: 'add', component: AddRoutineComponent },
   { path: 'update', component: UpdateRoutineComponent },
   { path: 'delete', component: DeleteRoutineComponent } */
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/routine/show-routine/show-routine.component.ts
+++ src/app/routine/show-routine/show-routine.component.ts
@@ -1,34 +1,104 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core'
+import { fromEvent } from 'rxjs'
+import { HasEventTargetAddRemove } from 'rxjs/internal/observable/fromEvent'
 
 @Component({
   selector: 'app-show-routine',
   templateUrl: './show-routine.component.html',
   styleUrls: ['./show-routine.component.css']
 })
-export class ShowRoutineComponent implements OnInit {
-  readonly initialRoutine = [
-    { id: '1', icon: '📚', title: 'Lee/Escribe/dibuja', start: '5:10', duration: '30' }, // min
-    { id: '2', icon: '🧘', title: 'Meditación', start: '5:50', duration: '30' },
-    { id: '3', icon: '🏋️', title: 'Ejercicio', start: '6:30', duration: '60' },
-    { id: '4', icon: '🍳', title: 'Alimentación', start: '7:40', duration: '30' },
-    { id: '5', icon: '⚙️', title: 'Proyectos', start: '9:00', duration: '180' },
-    { id: '6', icon: '🥗', title: 'Alimentación', start: '14:00', duration: '60' },
-    { id: '7', icon: '⚙️', title: 'Proyectos', start: '15:10', duration: '180' },
-    { id: '8', icon: '🍲', title: 'Alimentación', start: '18:30', duration: '40' },
-    { id: '9', icon: '🏃', title: 'Saltos cuerda 50*3', start: '19:20', duration: '10' },
-    { id: '10', icon: '🧖', title: 'Baño', start: '19:40', duration: '40' },
-    { id: '11', icon: '🧘', title: 'Meditación', start: '20:30', duration: '20' },
-    { id: '12', icon: '💤', title: 'Dormir', start: '21:00', duration: '480' }
-  ]
+export class ShowRoutineComponent implements OnInit, AfterViewInit {
+  routine: Activity [] = []
+  // @ViewChild('routine') routineList?: ElementRef
 
-  constructor () { }
+  /* constructor () {} */
 
   ngOnInit (): void {
-    /* this.currentTime()
-    setInterval(this.currentTime.bind(this), 1000) */
+    this.routine = this.getRoutine()
+    this.currentTime(this.routine)
+
+    setInterval(this.currentTime.bind(this, this.routine), 1000)
   }
 
-  calculateHoursAndMinutes = (min = 0) => {
+  ngAfterViewInit (): void {
+    const routineList = document.getElementById('routine')
+
+    const clickEventRoutineList = fromEvent(routineList as HasEventTargetAddRemove<ElementRef>, 'click')
+
+    clickEventRoutineList.subscribe((e: any) => {
+      if (e.target.nodeName !== 'UL') {
+        console.log(e.target.id)
+      }
+    })
+  }
+
+  showActivity (e: any): void {
+    console.log(e)
+  }
+
+  getRoutine (): Activity[] {
+    const routine = [
+      new Activity('1', '📚', 'Lee/Escribe/dibuja', '5:10', 30),
+      new Activity('2', '🧘', 'Meditación', '5:50', 30),
+      new Activity('3', '🏋️', 'Ejercicio', '6:30', 60),
+      new Activity('4', '🍳', 'Alimentación', '7:40', 30),
+      new Activity('5', '⚙️', 'Proyectos', '9:00', 190),
+      new Activity('6', '🥗', 'Alimentación', '14:00', 60),
+      new Activity('7', '⚙️', 'Proyectos', '15:10', 180),
+      new Activity('8', '🍲', 'Alimentación', '18:30', 40),
+      new Activity('9', '🏃', 'Saltos cuerda 50*3', '19:20', 10),
+      new Activity('10', '🧖', 'Baño', '19:40', 40),
+      new Activity('11', '🧘', 'Meditación', '20:30', 20),
+      new Activity('12', '💤', 'Dormir', '21:00', 480)
+    ]
+    return routine
+  }
+
+  currentTime (routine: Activity[] = []): any {
+    const date = new Date()
+    const dateObj = {
+      date: date,
+      month: date.getMonth(),
+      day: date.getDate(),
+      year: date.getFullYear()
+    }
+
+    // Filtra las posibles opciones dentro del rango de tiempo
+    const currentActivity = routine.filter(activity => this.identifyActivity(activity, dateObj))
+
+    if (currentActivity[0] === undefined) return
+
+    /* console.log(currentActivity[0].icon) */
+    const current = document.getElementById(currentActivity[0].id)
+
+    if (current != null) {
+      current.classList.add('activity--active')
+      /* current.style.backgroundColor = '#ccc'
+      current.style.fontSize = '2em'
+      current.style.color = '#fff' */
+    }
+    // TODO : quitar el estilo
+  }
+
+  identifyActivity (activity: Activity, dateObj: any): boolean {
+    const { date, month, day, year } = dateObj
+
+    const activityStartHour = activity.startHour?.split(':')
+    const hour = Number(activityStartHour[0])
+    const min = Number(activityStartHour[1])
+
+    const duration = this.calcTime(activity.durationMins + min)
+
+    const timeActivityStart = new Date(year, month, day, hour, min, 0, 0)
+    const timeActivityEnd = new Date(year, month, day, duration.hours + hour, duration.min, 0, 0)
+
+    const start = date.getTime() > timeActivityStart.getTime()
+    const end = date.getTime() < timeActivityEnd.getTime()
+
+    return end && start
+  }
+
+  calcTime (min: number): { hours: number, min: number} {
     let hours = 0
     while (min > 59) {
       min = min - 60
@@ -36,39 +106,14 @@ export class ShowRoutineComponent implements OnInit {
     }
     return { hours, min }
   }
+}
 
-  currentTime () {
-    const date = new Date()
-
-    const month = date.getMonth()
-    const day = date.getDate()
-    const year = date.getFullYear()
-
-    // Filtra las posibles opciones dentro del rango de tiempo
-    const currentActivity = this.initialRoutine.filter((activity) => {
-      const activityStart = activity.start.split(':')
-
-      const durationTime = this.calculateHoursAndMinutes(Number(activity.duration) + Number(activityStart[1]))
-
-      const timeActivityStart = new Date(year, month, day, Number(activityStart[0]), Number(activityStart[1]), 0, 0)
-      const timeActivityEnd = new Date(year, month, day, durationTime.hours + Number(activityStart[0]), durationTime.min, 0, 0)
-
-      const start = date.getTime() > timeActivityStart.getTime()
-      const end = date.getTime() < timeActivityEnd.getTime()
-
-      return end && start
-    })
-
-    console.log(currentActivity[0].icon)
-
-    // TODO : quitar el estilo
-    /* if (currentActivity[0]) {
-    const current = $(currentActivity[0].id)
-    current.style.padding = '3em'
-    current.style.display = 'block'
-    current.style.backgroundColor = 'yellow'
-    current.style.fontSize = '2em'
-    current.style.color = '#000'
-    } */
-  }
+class Activity {
+  constructor (
+    public id: string,
+    public icon: string,
+    public title: string,
+    public startHour: string,
+    public durationMins: number
+  ) { }
 }
